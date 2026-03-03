3f5949ab404ba7b658255d37c432be77e65f2038
Valentina Pruteanu <B00176037@myTUDublin.ie>
2026-03-03 17:43:26 +0000

    Fix SvelteKit GitHub Pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
       pages: 'dist',
-      assets: 'dist'
+      assets: 'dist',
+      fallback: '404.html'
     }),
     paths: {
       base: process.env.NODE_ENV === 'production'
